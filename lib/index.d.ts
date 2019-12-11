7e0eb4b42091b5a3c0c72103437bc38a76b41575
Jack Allen <jackallensemail+git@gmail.com>
2019-12-11 18:57:00 +0000

    clean

--- lib/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { default as splitReducers } from './splitReducers';
--- lib/splitReducers.d.ts
+++
@@ -1,3 +0,0 @@
-import { Action, Reducer } from 'redux';
-declare function splitReducers<S, A extends Action>(reducers: Reducer<S, A>[]): (state: S, action: A) => S;
-export default splitReducers;
